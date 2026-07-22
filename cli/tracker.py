69a8177b389a6b95f210dc16bcbdd727975aae61
Micah Mitchell <micahmitchell101@gmail.com>
2026-07-22 01:09:05 -0400

    Adding Click commands and options

--- cli/tracker.py
+++ cli/tracker.py
@@ -1,4 +1,17 @@
 import click
 from rich.console import Console
 from rich.table import Table
+from core.expenses import addExpense, loadExpense, deleteExpense, summaryExpenses
+
+console = Console()
+
+@click.group()
+def cli():
+    pass
+
+if __name__ == "__main__":
+    cli()
+
+@click.command()
+@click.option('--amount', prompt='Amount', type=float, help='The amount of the expense')
 
