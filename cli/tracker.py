e2acd0f05604171c22df4fe0a0cf2ce6b33297e6
Micah Mitchell <micahmitchell101@gmail.com>
2026-07-22 23:50:26 -0400

    Added command group and add command, linked it to the addExpense function from expenses.py

--- cli/tracker.py
+++ cli/tracker.py
@@ -9,9 +9,22 @@ console = Console()
 def cli():
     pass
 
+######
+# Command for the addExpense function
+######
+@cli.command()
+
+# Options for the arguments the command takes, each required because each contains prompt
+@click.option('--amount', prompt='Amount', type=float, help='The amount of the expense')
+@click.option('--category', prompt='Category', type=str, help='The category of the expense')
+@click.option('--description', prompt='Description', type=str, help='The description of the expense')
+
+# Add command is defined with arguments that are linked to the options, arguments autmatically find options based on "--{argument}" notation
+def add(amount, category, description):
+    addExpense(amount, category, description)
+    console.print("Expense has been added to records!")
+
+
+
 if __name__ == "__main__":
     cli()
-
-@click.command()
-@click.option('--amount', prompt='Amount', type=float, help='The amount of the expense')
-
